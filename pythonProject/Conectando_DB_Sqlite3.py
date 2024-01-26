7921856d70052ef049fa433a713444c0d960a859
Rodrigo <rodrigo2013.rs33@gmail.com>
2024-01-26 12:28:54 -0300

    script para update db

--- pythonProject/Conectando_DB_Sqlite3.py
+++ pythonProject/Conectando_DB_Sqlite3.py
@@ -2,17 +2,24 @@ import pandas as pd
 import sqlite3
 
 conn = sqlite3.connect('/home/rodrigo/git_workplace/OpenClassRoom/django-web-app/merchex/db.sqlite3')
+
 cursor = conn.cursor()
 
+cursor.execute('SELECT name FROM listings_singers')
+
 data = pd.read_csv('/home/rodrigo/git_workplace/PythonProjects/pythonProject/bandas2.csv')
-lista = data['artist'].unique()
 
-# print(lista)
-for i, cantor in enumerate(lista):
+df = data.drop_duplicates(subset='artist')
 
-    cursor.execute('INSERT OR IGNORE INTO listings_singers (id, name) VALUES (?, ?)', (i + 1, cantor))
+df = df[['artist', 'status', 'description', 'genre', 'site', 'year']]
+
+singers = df[~df['artist'].isin(['James Arthur', 'Labrinth'])]
+# print(singers['status'])
+for i, cantor in enumerate(singers['artist']):
+    #print(i)
+    cursor.execute(
+        f"UPDATE listings_singers SET year_formed = {int(singers.iloc[i, 5])} WHERE name = '{cantor}'")
 
 conn.commit()
 
 conn.close()
-
